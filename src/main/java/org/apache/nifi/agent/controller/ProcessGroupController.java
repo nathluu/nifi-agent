c9a95e3dc5260502d91a90dd52cf784bcef5bdf0
nathluu <luuhoangtanbis@gmail.com>
2022-04-21 16:00:02 +0700

    Fix update parameter context

--- src/main/java/org/apache/nifi/agent/controller/ProcessGroupController.java
+++ src/main/java/org/apache/nifi/agent/controller/ProcessGroupController.java
@@ -1,6 +1,7 @@
 package org.apache.nifi.agent.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.nifi.agent.dto.NiFiAgentParameterDTO;
 import org.apache.nifi.agent.dto.NiFiAgentProcessGroupDTO;
 import org.apache.nifi.agent.dto.NiFiAgentProcessGroupsDTO;
@@ -9,6 +10,7 @@ import org.apache.nifi.agent.exception.NiFiClientException;
 import org.apache.nifi.agent.service.NiFiClient;
 import org.apache.nifi.agent.service.ParamContextClient;
 import org.apache.nifi.agent.util.Converter;
+import org.apache.nifi.agent.util.ProcessGroupHandler;
 import org.apache.nifi.web.api.dto.ParameterContextDTO;
 import org.apache.nifi.web.api.dto.ParameterDTO;
 import org.apache.nifi.web.api.entity.*;
@@ -16,9 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Slf4j
 @RestController
@@ -44,49 +44,8 @@ public class ProcessGroupController {
     @PostMapping("/process-groups/{id}/process-groups/upload")
     NiFiAgentProcessGroupDTO uploadProcessGroup(@PathVariable("id") String parentGroupId, @RequestBody PGUploadRequestDTO req)
             throws NiFiClientException, IOException, InterruptedException {
-        final ParamContextClient paramContextClient = client.getParamContextClient();
         final ProcessGroupEntity result = client.getProcessGroupClient().uploadProcessGroup(parentGroupId, req);
-        final String paramContextId = result.getParameterContext().getId();
-        ParameterContextEntity existingParameterContextEntity = paramContextClient.getParamContext(paramContextId, false);
-        ParameterContextDTO existingParameterContextDTO = existingParameterContextEntity.getComponent();
-        List<NiFiAgentParameterDTO> parameters = req.getParameters();
-        for (NiFiAgentParameterDTO param : parameters) {
-            String paramName = param.getName();
-            String paramValue = param.getValue();
-            Optional<ParameterDTO> existingParam = existingParameterContextDTO.getParameters().stream()
-                    .map(ParameterEntity::getParameter)
-                    .filter(p -> p.getName().equals(paramName))
-                    .findFirst();
-
-            if (!existingParam.isPresent() && paramValue == null) {
-                throw new IllegalArgumentException("A parameter value is required when creating a new parameter");
-            }
-
-            if (existingParam.isPresent() && existingParam.get().getValue().equals(paramValue)) {
-//                throw new IllegalArgumentException(String.format("Parameter value supplied for parameter [%s] is the same as its current value", paramName));
-                continue;
-            }
-            // Construct the objects for the update...
-            ParameterDTO parameterDTO = existingParam.isPresent() ? existingParam.get() : new ParameterDTO();
-            parameterDTO.setName(paramName);
-            if (paramValue != null) {
-                parameterDTO.setValue(paramValue);
-            }
-
-            ParameterEntity parameterEntity = new ParameterEntity();
-            parameterEntity.setParameter(parameterDTO);
-            ParameterContextDTO parameterContextDTO = new ParameterContextDTO();
-            parameterContextDTO.setId(existingParameterContextEntity.getId());
-            parameterContextDTO.setParameters(Collections.singleton(parameterEntity));
-
-            ParameterContextEntity updatedParameterContextEntity = new ParameterContextEntity();
-            updatedParameterContextEntity.setId(paramContextId);
-            updatedParameterContextEntity.setComponent(parameterContextDTO);
-            updatedParameterContextEntity.setRevision(existingParameterContextEntity.getRevision());
-            ParameterContextUpdateRequestEntity updateRequestEntity = paramContextClient.updateParamContext(updatedParameterContextEntity);
-            Thread.sleep(2000);
-        }
-
+        ProcessGroupHandler.updateProcessGroupParameterContext(client, result, req.getParameters());
         return Converter.convertProcessGroupEntityToDto(client.getProcessGroupClient().getProcessGroup(result.getId()));
     }
 }
